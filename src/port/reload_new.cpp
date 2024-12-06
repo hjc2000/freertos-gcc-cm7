815a35fc2f9bfb0538b8f40ae060121ecb1069dc
hjc2000 <hjc.666@foxmail.com>
2024-12-06 09:59:24 +0800

    reload_new会禁用中断再操作堆

--- src/port/reload_new.cpp
+++ src/port/reload_new.cpp
@@ -1,66 +1,89 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include <cstddef>
+#include <hal.h>
 #include <new>
 
 void *operator new(size_t size)
 {
-	void *ptr = pvPortMalloc(size);
-	if (ptr == nullptr)
-	{
-		throw std::bad_alloc();
-	}
+    __disable_irq();
+    void *ptr = pvPortMalloc(size);
+    __enable_irq();
+    if (ptr == nullptr)
+    {
+        throw std::bad_alloc();
+    }
 
-	return ptr;
+    return ptr;
 }
 
 void *operator new[](size_t size)
 {
-	void *ptr = pvPortMalloc(size);
-	if (ptr == nullptr)
-	{
-		throw std::bad_alloc();
-	}
+    __disable_irq();
+    void *ptr = pvPortMalloc(size);
+    __enable_irq();
+    if (ptr == nullptr)
+    {
+        throw std::bad_alloc();
+    }
 
-	return ptr;
+    return ptr;
 }
 
 void operator delete(void *ptr) noexcept
 {
-	vPortFree(ptr);
+    __disable_irq();
+    vPortFree(ptr);
+    __enable_irq();
 }
 
 void operator delete[](void *ptr) noexcept
 {
-	vPortFree(ptr);
+    __disable_irq();
+    vPortFree(ptr);
+    __enable_irq();
 }
 
 void *operator new(size_t size, std::nothrow_t const &) noexcept
 {
-	return pvPortMalloc(size);
+    __disable_irq();
+    void *p = pvPortMalloc(size);
+    __enable_irq();
+    return p;
 }
 
 void *operator new[](size_t size, std::nothrow_t const &) noexcept
 {
-	return pvPortMalloc(size);
+    __disable_irq();
+    void *p = pvPortMalloc(size);
+    __enable_irq();
+    return p;
 }
 
 void operator delete(void *ptr, std::nothrow_t const &) noexcept
 {
-	vPortFree(ptr);
+    __disable_irq();
+    vPortFree(ptr);
+    __enable_irq();
 }
 
 void operator delete[](void *ptr, std::nothrow_t const &) noexcept
 {
-	vPortFree(ptr);
+    __disable_irq();
+    vPortFree(ptr);
+    __enable_irq();
 }
 
 void operator delete(void *ptr, size_t size) noexcept
 {
-	vPortFree(ptr);
+    __disable_irq();
+    vPortFree(ptr);
+    __enable_irq();
 }
 
 void operator delete[](void *ptr, size_t size) noexcept
 {
-	vPortFree(ptr);
+    __disable_irq();
+    vPortFree(ptr);
+    __enable_irq();
 }
