428ff13ed1cb60de081163c2d963b1bdddd9c793
hjc2000 <hjc.666@foxmail.com>
2025-01-10 11:42:42 +0800

    #pragma region 常量

--- src/MemMang/heap_4.cpp
+++ src/MemMang/heap_4.cpp
@@ -41,50 +41,6 @@ namespace
     /// 很低，链表节点本身占用的内存都比指向的内存块大了，那这块内存就不值得用一个链表节点去指向它。
     size_t constexpr _heap_minimum_block_size = ((size_t)(_size_of_heap_block_linklist_element << 1));
 
-    /// @brief 假设 1 个字节有 8 位。
-    size_t constexpr _bit_count_per_byte = ((size_t)8);
-
-    /* Max value that fits in a size_t type. */
-    size_t constexpr heapSIZE_MAX = (~((size_t)0));
-
-    /* Check if multiplying a and b will result in overflow. */
-    bool constexpr heapMULTIPLY_WILL_OVERFLOW(size_t a, size_t b)
-    {
-        return (((a) > 0) && ((b) > (heapSIZE_MAX / (a))));
-    }
-
-    /* Check if adding a and b will result in overflow. */
-    bool constexpr heapADD_WILL_OVERFLOW(size_t a, size_t b)
-    {
-        return ((a) > (heapSIZE_MAX - (b)));
-    }
-
-    /* MSB of the _size member of an BlockLink_t structure is used to track
-     * the allocation status of a block.  When MSB of the _size member of
-     * an BlockLink_t structure is set then the block belongs to the application.
-     * When the bit is free the block is still part of the free heap space. */
-    size_t constexpr heapBLOCK_ALLOCATED_BITMASK = ((size_t)1) << ((sizeof(size_t) * _bit_count_per_byte) - 1);
-
-    bool constexpr heapBLOCK_SIZE_IS_VALID(size_t _size)
-    {
-        return ((_size)&heapBLOCK_ALLOCATED_BITMASK) == 0;
-    }
-
-    bool constexpr heapBLOCK_IS_ALLOCATED(freertos::BlockLink_t *pxBlock)
-    {
-        return ((pxBlock->_size) & heapBLOCK_ALLOCATED_BITMASK) != 0;
-    }
-
-    void constexpr heapALLOCATE_BLOCK(freertos::BlockLink_t *pxBlock)
-    {
-        (pxBlock->_size) |= heapBLOCK_ALLOCATED_BITMASK;
-    }
-
-    void constexpr heapFREE_BLOCK(freertos::BlockLink_t *pxBlock)
-    {
-        (pxBlock->_size) &= ~heapBLOCK_ALLOCATED_BITMASK;
-    }
-
     uint8_t _buffer[configTOTAL_HEAP_SIZE];
     freertos::Heap4 _heap4{_buffer, sizeof(_buffer)};
 
@@ -147,7 +103,7 @@ void freertos::Heap4::InsertBlockIntoFreeList(freertos::BlockLink_t *pxBlockToIn
 
     /* Iterate through the list until a block is found that has a higher address
      * than the block being inserted. */
-    for (pxIterator = &xStart; pxIterator->_next_free_block < pxBlockToInsert; pxIterator = pxIterator->_next_free_block)
+    for (pxIterator = &_head_element; pxIterator->_next_free_block < pxBlockToInsert; pxIterator = pxIterator->_next_free_block)
     {
         /* Nothing to do here, just iterate to the right position. */
     }
@@ -172,7 +128,7 @@ void freertos::Heap4::InsertBlockIntoFreeList(freertos::BlockLink_t *pxBlockToIn
 
     if ((puc + pxBlockToInsert->_size) == (uint8_t *)pxIterator->_next_free_block)
     {
-        if (pxIterator->_next_free_block != pxEnd)
+        if (pxIterator->_next_free_block != _tail_element)
         {
             /* Form one big block from the two blocks. */
             pxBlockToInsert->_size += pxIterator->_next_free_block->_size;
@@ -180,7 +136,7 @@ void freertos::Heap4::InsertBlockIntoFreeList(freertos::BlockLink_t *pxBlockToIn
         }
         else
         {
-            pxBlockToInsert->_next_free_block = pxEnd;
+            pxBlockToInsert->_next_free_block = _tail_element;
         }
     }
     else
@@ -221,25 +177,25 @@ freertos::Heap4::Heap4(uint8_t *buffer, size_t size)
 
     pucAlignedHeap = (uint8_t *)uxAddress;
 
-    /* xStart is used to hold a pointer to the first item in the list of free
+    /* _head_element is used to hold a pointer to the first item in the list of free
      * blocks.  The void cast is used to prevent compiler warnings. */
-    xStart._next_free_block = (BlockLink_t *)pucAlignedHeap;
-    xStart._size = (size_t)0;
+    _head_element._next_free_block = (BlockLink_t *)pucAlignedHeap;
+    _head_element._size = (size_t)0;
 
-    /* pxEnd is used to mark the end of the list of free blocks and is inserted
+    /* _tail_element is used to mark the end of the list of free blocks and is inserted
      * at the end of the heap space. */
     uxAddress = ((portPOINTER_SIZE_TYPE)pucAlignedHeap) + xTotalHeapSize;
     uxAddress -= _size_of_heap_block_linklist_element;
     uxAddress &= ~((portPOINTER_SIZE_TYPE)portBYTE_ALIGNMENT_MASK);
-    pxEnd = (BlockLink_t *)uxAddress;
-    pxEnd->_size = 0;
-    pxEnd->_next_free_block = NULL;
+    _tail_element = (BlockLink_t *)uxAddress;
+    _tail_element->_size = 0;
+    _tail_element->_next_free_block = NULL;
 
     /* To start with there is a single free block that is sized to take up the
-     * entire heap space, minus the space taken by pxEnd. */
+     * entire heap space, minus the space taken by _tail_element. */
     pxFirstFreeBlock = (BlockLink_t *)pucAlignedHeap;
     pxFirstFreeBlock->_size = (size_t)(uxAddress - (portPOINTER_SIZE_TYPE)pxFirstFreeBlock);
-    pxFirstFreeBlock->_next_free_block = pxEnd;
+    pxFirstFreeBlock->_next_free_block = _tail_element;
 
     /* Only one block exists - and it covers the entire usable heap space. */
     xMinimumEverFreeBytesRemaining = pxFirstFreeBlock->_size;
@@ -258,7 +214,7 @@ void *freertos::Heap4::Malloc(size_t xWantedSize)
     {
         /* If this is the first call to malloc then the heap will require
          * initialisation to setup the list of free blocks. */
-        if (pxEnd == NULL)
+        if (_tail_element == NULL)
         {
             // 已被构造函数取代。
             //  prvHeapInit();
@@ -275,7 +231,7 @@ void *freertos::Heap4::Malloc(size_t xWantedSize)
              * additional increment may also be needed for alignment. */
             xAdditionalRequiredSize = _size_of_heap_block_linklist_element + portBYTE_ALIGNMENT - (xWantedSize & portBYTE_ALIGNMENT_MASK);
 
-            if (!heapADD_WILL_OVERFLOW(xWantedSize, xAdditionalRequiredSize))
+            if (!HeapAddWillOverflow(xWantedSize, xAdditionalRequiredSize))
             {
                 xWantedSize += xAdditionalRequiredSize;
             }
@@ -293,14 +249,14 @@ void *freertos::Heap4::Malloc(size_t xWantedSize)
          * top bit is set.  The top bit of the block size member of the BlockLink_t
          * structure is used to determine who owns the block - the application or
          * the kernel, so it must be free. */
-        if (heapBLOCK_SIZE_IS_VALID(xWantedSize))
+        if (HeapBlockSizeIsValid(xWantedSize))
         {
             if ((xWantedSize > 0) && (xWantedSize <= xFreeBytesRemaining))
             {
                 /* Traverse the list from the start (lowest address) block until
                  * one of adequate size is found. */
-                pxPreviousBlock = &xStart;
-                pxBlock = xStart._next_free_block;
+                pxPreviousBlock = &_head_element;
+                pxBlock = _head_element._next_free_block;
 
                 while ((pxBlock->_size < xWantedSize) && (pxBlock->_next_free_block != NULL))
                 {
@@ -310,7 +266,7 @@ void *freertos::Heap4::Malloc(size_t xWantedSize)
 
                 /* If the end marker was reached then a block of adequate size
                  * was not found. */
-                if (pxBlock != pxEnd)
+                if (pxBlock != _tail_element)
                 {
                     /* Return the memory space pointed to - jumping over the
                      * BlockLink_t structure at its start. */
@@ -357,7 +313,7 @@ void *freertos::Heap4::Malloc(size_t xWantedSize)
 
                     /* The block is being returned - it is allocated and owned
                      * by the application and has no "next" block. */
-                    heapALLOCATE_BLOCK(pxBlock);
+                    HeapAllocateBlock(pxBlock);
                     pxBlock->_next_free_block = NULL;
                     xNumberOfSuccessfulAllocations++;
                 }
@@ -411,16 +367,16 @@ void freertos::Heap4::Free(void *pv)
         /* This casting is to keep the compiler from issuing warnings. */
         pxLink = (freertos::BlockLink_t *)puc;
 
-        configASSERT(heapBLOCK_IS_ALLOCATED(pxLink));
+        configASSERT(HeapBlockIsAllocated(pxLink));
         configASSERT(pxLink->_next_free_block == NULL);
 
-        if (heapBLOCK_IS_ALLOCATED(pxLink))
+        if (HeapBlockIsAllocated(pxLink))
         {
             if (pxLink->_next_free_block == NULL)
             {
                 /* The block is being returned to the heap - it is no longer
                  * allocated. */
-                heapFREE_BLOCK(pxLink);
+                HeapFreeBlock(pxLink);
 #if (configHEAP_CLEAR_MEMORY_ON_FREE == 1)
                 {
                     (void)memset(puc + _size_of_heap_block_linklist_element, 0, pxLink->_size - _size_of_heap_block_linklist_element);
@@ -453,7 +409,7 @@ void *freertos::Heap4::Calloc(size_t xNum, size_t xSize)
 {
     void *pv = NULL;
 
-    if (!heapMULTIPLY_WILL_OVERFLOW(xNum, xSize))
+    if (!HeapMultiplyWillOverflow(xNum, xSize))
     {
         pv = pvPortMalloc(xNum * xSize);
 
@@ -473,13 +429,13 @@ void freertos::Heap4::GetHeapStats(xHeapStats *pxHeapStats)
 
     vTaskSuspendAll();
     {
-        pxBlock = xStart._next_free_block;
+        pxBlock = _head_element._next_free_block;
 
         /* pxBlock will be NULL if the heap has not been initialised.  The heap
          * is initialised automatically when the first allocation is made. */
         if (pxBlock != NULL)
         {
-            while (pxBlock != pxEnd)
+            while (pxBlock != _tail_element)
             {
                 /* Increment the number of blocks and record the largest block seen
                  * so far. */
--- src/MemMang/heap_4.h
+++ src/MemMang/heap_4.h
@@ -20,6 +20,53 @@ namespace freertos
     class Heap4
     {
     private:
+#pragma region 常量
+        /// @brief 假设 1 个字节有 8 位。
+        static size_t constexpr _bit_count_per_byte = (size_t)8;
+
+        /* Max value that fits in a size_t type. */
+        static size_t constexpr _heap_size_max = ~((size_t)0);
+
+        /* Check if multiplying a and b will result in overflow. */
+        bool constexpr HeapMultiplyWillOverflow(size_t a, size_t b)
+        {
+            return ((a) > 0) && ((b) > (_heap_size_max / (a)));
+        }
+
+        /* Check if adding a and b will result in overflow. */
+        bool constexpr HeapAddWillOverflow(size_t a, size_t b)
+        {
+            return (a) > (_heap_size_max - (b));
+        }
+
+        /* MSB of the _size member of an BlockLink_t structure is used to track
+         * the allocation status of a block.  When MSB of the _size member of
+         * an BlockLink_t structure is set then the block belongs to the application.
+         * When the bit is free the block is still part of the free heap space. */
+        static size_t constexpr _heap_block_allocated_bitmask = ((size_t)1) << ((sizeof(size_t) * _bit_count_per_byte) - 1);
+
+        bool constexpr HeapBlockSizeIsValid(size_t _size)
+        {
+            return ((_size)&_heap_block_allocated_bitmask) == 0;
+        }
+
+        bool constexpr HeapBlockIsAllocated(freertos::BlockLink_t *pxBlock)
+        {
+            return ((pxBlock->_size) & _heap_block_allocated_bitmask) != 0;
+        }
+
+        void constexpr HeapAllocateBlock(freertos::BlockLink_t *pxBlock)
+        {
+            (pxBlock->_size) |= _heap_block_allocated_bitmask;
+        }
+
+        void constexpr HeapFreeBlock(freertos::BlockLink_t *pxBlock)
+        {
+            (pxBlock->_size) &= ~_heap_block_allocated_bitmask;
+        }
+
+#pragma endregion
+
         /// @brief 将被释放的内存插入链表。
         /// @note 如果发现与链表中要插入位置的前一个节点和后一个节点指向的内存是连续的，会合并这些节点。
         /// @param pxBlockToInsert
@@ -32,8 +79,8 @@ namespace freertos
         Heap4(uint8_t *buffer, size_t size);
 
         /* Create a couple of list links to mark the start and end of the list. */
-        BlockLink_t xStart;
-        BlockLink_t *pxEnd = nullptr;
+        BlockLink_t _head_element;
+        BlockLink_t *_tail_element = nullptr;
 
         /* Keeps track of the number of calls to allocate and free memory as well as the
          * number of free bytes remaining, but says nothing about fragmentation. */
